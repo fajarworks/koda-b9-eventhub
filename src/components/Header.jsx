b569b6946c0802b430db21754b1e7d92e3a236ec
fajarworks <muhammad.fajarr362@gmail.com>
2026-08-15 18:24:30 +0700

    feat: update header use navlink for active navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,51 +1,46 @@
 // import React from 'react';
 
 import { FiMoon } from "react-icons/fi";
-import { Link, useLocation } from "react-router";
+import { NavLink } from "react-router";
+import Logo from "./Logo";
 function Header() {
-  const location = useLocation();
 
   return (
     <>
       <header className="w-full p-6 bg-white border-b border-gray-300">
         <nav className="flex mx-auto max-w-7xl justify-between">
           <div className="flex  my-center gap-2">
-            <div className="flex gap-2 my-center cursor-pointer">
-              <div className=" w-7 h-7 flex my-center rounded-md  bg-primary font-medium text-white cursor-pointe">
-                <p>E</p>
-              </div>
-              <p className="font-bold">EventHub</p>
-            </div>
+            <Logo className="text-black" />
             <ul className="flex text-color-text my-center gap-2">
-              <Link
-                to="/events"
-                className={
-                  location.pathname === "/events"
-                    ? "nav-active px-3 py-1.5"
-                    : "text-color-text px-3 py-1.5 "
-                }
-              >
-                Events
-              </Link>
+              <li>
+                <NavLink to="/explore">
+                  {({ isActive }) => (
+                    <span className={isActive ? "nav-active px-3 py-1.5" : "text-gray px-3 py-1.5"}>Explore</span>
+                    )}
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/events">
+                  {({ isActive }) => (
+                    <span className={isActive ? "nav-active px-3 py-1.5" : "text-gray px-3 py-1.5"}>Event</span>
+                    )}
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/communities">
+                  {({ isActive }) => (
+                    <span className={isActive ? "nav-active px-3 py-1.5" : "text-gray px-3 py-1.5"}>Communities</span>
+                    )}
+                </NavLink>
+              </li>
 
-              <Link
-                to="/communities"
-                className={
-                  location.pathname === "/communities"
-                    ? "nav-active px-3 py-1.5"
-                    : "text-color-text px-3 py-1.5"
-                }
-              >
-                Communities
-              </Link>
-              <Link></Link>
             </ul>
           </div>
 
           <div className="flex my-center gap-2">
             <p className="text-color-text pr-1">browsing as guest</p>
             <div className="p-2 cursor-pointer">
-              <FiMoon size={24}/>
+              <FiMoon size={24} />
             </div>
             <button className="px-4 py-1.5 rounded-lg text-white bg-primary cursor-pointer">
               Sign In
