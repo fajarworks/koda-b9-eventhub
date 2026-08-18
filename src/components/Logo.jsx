c8e2cf72d7cdd1af321cc937b1cffdfd832e4f03
fajarworks <muhammad.fajarr362@gmail.com>
2026-08-18 10:33:56 +0700

    feat: wrap Logo component in Link to enable navigation to explore page

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 
 
 function Logo({className = ""}) {
   return (
-    <div>
+    <Link to={"/explore"}>
       <div className="flex gap-2 items-center my-2 cursor-pointer">
         <div className=" w-7 h-7 flex my-center rounded-md  bg-primary font-medium text-white cursor-pointe">
           <p>E</p>
         </div>
         <p className={`font-bold ${className}`}>EventHub</p>
       </div>
-    </div>
+    </Link>
   );
 }
 
