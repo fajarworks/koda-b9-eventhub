a9a7c7745008e774a89f020eb7d1aeece2182f43
fajarworks <muhammad.fajarr362@gmail.com>
2026-08-18 17:34:39 +0700

    feat: decrease padding y

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -22,10 +22,7 @@ function Register() {
   } = useForm();
 
   const onSubmit = (data) => {
-    // const users = JSON.parse(localStorage.getItem("users")) || [];
     setUser([...user,data])
-    // users.push(data);
-    // localStorage.setItem("users", JSON.stringify(users));
     navigate("/auth/login");
   };
 
@@ -35,7 +32,7 @@ function Register() {
         <Logo className="text-black" />
       </div>
       <div className="w-full">
-        <div className="flex flex-col gap-1 sm:gap-2">
+        <div className="flex flex-col gap-0.5 sm:gap-2">
           <h1 className="font-semibold text-lg sm:text-2xl lg:text-3xl">
             Create your account
           </h1>
@@ -54,13 +51,13 @@ function Register() {
         <div className="flex w-full gap-2 sm:gap-3 text-color-text pt-3 sm:pt-6">
           <button
             type="button"
-            className="w-full border border-gray-300 rounded-lg bg-white px-2 sm:px-4 py-1.5 sm:py-2.5 text-xs sm:text-base hover:bg-gray-50 transition"
+            className="w-full border border-gray-300 rounded-lg bg-white px-2 sm:px-4 py-1.5 sm:py-1.5 text-xs sm:text-base hover:bg-gray-50 transition"
           >
             Google
           </button>
           <button
             type="button"
-            className="w-full border border-gray-300 rounded-lg bg-white px-2 sm:px-4 py-1.5 sm:py-2.5 text-xs sm:text-base hover:bg-gray-50 transition"
+            className="w-full border border-gray-300 rounded-lg bg-white px-2 sm:px-4 py-1.5 sm:py-1.5 text-xs sm:text-base hover:bg-gray-50 transition"
           >
             Github
           </button>
@@ -77,7 +74,7 @@ function Register() {
           onSubmit={handleSubmit(onSubmit)}
           className="flex flex-col gap-2 sm:gap-4"
         >
-          <div className="flex flex-col gap-1 sm:gap-2">
+          <div className="flex flex-col gap-0.5 sm:gap-2">
             <label
               htmlFor="fullname"
               className="text-xs sm:text-sm font-medium"
@@ -88,15 +85,15 @@ function Register() {
               {...register("fullName", { required: true })}
               type="text"
               id="fullname"
-              className="text-color-text border border-gray-300 w-full px-3 py-1.5 sm:py-2.5 text-sm sm:text-base rounded-lg focus:outline-none focus:ring-2 focus:ring-primary/20 focus:border-primary"
+              className="text-color-text border border-gray-300 w-full px-3 py-1.5 sm:py-1.5 text-sm sm:text-base rounded-lg focus:outline-none focus:ring-2 focus:ring-primary/20 focus:border-primary"
             />
             {errors.fullName?.type === "required" && (
-              <p role="alert" className="text-red-500">
+              <p role="alert" className="text-red-500 text-sm">
                 Full name is required
               </p>
             )}
           </div>
-          <div className="flex flex-col gap-1 sm:gap-2">
+          <div className="flex flex-col gap-0.5 sm:gap-2">
             <label htmlFor="email" className="text-xs sm:text-sm font-medium">
               Email address
             </label>
@@ -104,8 +101,6 @@ function Register() {
               {...register("email", {
                 required: "email can't be empty",
                 validate: (value) => {
-                  // const user = JSON.parse(localStorage.getItem("users")) || [];
-
                   const emailExist = user.some(
                     (user) => user.email.toLowerCase() === value.toLowerCase(),
                   );
@@ -114,13 +109,13 @@ function Register() {
               })}
               type="email"
               id="email"
-              className="text-color-text border border-gray-300 w-full px-3 py-1.5 sm:py-2.5 text-sm sm:text-base rounded-lg focus:outline-none focus:ring-2 focus:ring-primary/20 focus:border-primary"
+              className="text-color-text border border-gray-300 w-full px-3 py-1.5 sm:py-1.5 text-sm sm:text-base rounded-lg focus:outline-none focus:ring-2 focus:ring-primary/20 focus:border-primary"
             />
             {errors.email && (
-              <p className="text-red-500">{errors.email.message}</p>
+              <p className="text-red-500 text-sm">{errors.email.message}</p>
             )}
           </div>
-          <div className="flex flex-col gap-1 sm:gap-2">
+          <div className="flex flex-col gap-0.5 sm:gap-2">
             <label
               htmlFor="password"
               className="text-xs sm:text-sm font-medium"
@@ -138,7 +133,7 @@ function Register() {
                 })}
                 type={showPassword ? "text" : "password" }
                 id="password"
-                className="text-color-text border border-gray-300 w-full px-3 py-1.5 sm:py-2.5 pr-8 sm:pr-10 text-sm sm:text-base rounded-lg focus:outline-none focus:ring-2 focus:ring-primary/20 focus:border-primary"
+                className="text-color-text border border-gray-300 w-full px-3 py-1.5 sm:py-1.5 pr-8 sm:pr-10 text-sm sm:text-base rounded-lg focus:outline-none focus:ring-2 focus:ring-primary/20 focus:border-primary"
               />
               <span
                 onClick={handleShowPass}
@@ -148,12 +143,12 @@ function Register() {
               </span>
             </div>
             {errors.password && (
-              <p role="alert" className="text-red-500">
+              <p role="alert" className="text-red-500 text-sm">
                 {errors.password.message}
               </p>
             )}
           </div>
-          <div className="flex flex-col gap-1 sm:gap-2">
+          <div className="flex flex-col gap-0.5 sm:gap-2">
             <label
               htmlFor="confirmPassword"
               className="text-xs sm:text-sm font-medium"
@@ -168,27 +163,31 @@ function Register() {
                     value === getValues("password") || "password doesn't match",
                 })}
                 id="confirmPassword"
-                className="text-color-text border border-gray-300 w-full px-3 py-1.5 sm:py-2.5 pr-8 sm:pr-10 text-sm sm:text-base rounded-lg focus:outline-none focus:ring-2 focus:ring-primary/20 focus:border-primary"
+                className="text-color-text border border-gray-300 w-full px-3 py-1.5 sm:py-1.5 pr-8 sm:pr-10 text-sm sm:text-base rounded-lg focus:outline-none focus:ring-2 focus:ring-primary/20 focus:border-primary"
               />
               <span className="absolute top-1/2 -translate-y-1/2 right-2 sm:right-4 cursor-pointer text-gray-500 w-4 sm:w-5 h-4 sm:h-5">
                 {showPassword ? <FiEyeOff /> : <FiEye />}
               </span>
             </div>
             {errors.confirmPassword && (
-              <p className="text-red-500">{errors.confirmPassword.message}</p>
+              <p className="text-red-500 text-sm">{errors.confirmPassword.message}</p>
             )}
           </div>
           <div className="text-color-text flex gap-2 py-2">
-            <input type="checkbox" id="terms" name="terms" required />
+            <input type="checkbox" id="terms" {...register("terms", {required: "please check the 'terms of service and privacy and policy' to proceed"})} required/>
             <label htmlFor="terms">
               I agree to the{" "}
               <Link className="text-primary">Terms of Service </Link>and{" "}
               <Link className="text-primary">Privacy and Policy</Link>
             </label>
           </div>
+            {errors.terms && (
+              <p className="text-red-500 text-sm">{ errors.terms.message}</p>
+            )
+              }
           <button
             type="submit"
-            className="w-full px-3 py-1.5 sm:py-2.5 bg-primary rounded-lg text-white cursor-pointer hover:opacity-90 transition text-sm sm:text-base mt-1"
+            className="w-full px-3 py-1.5 sm:py-1.5 bg-primary rounded-lg text-white cursor-pointer hover:opacity-90 transition text-sm sm:text-base mt-1"
           >
             Create Account
           </button>
