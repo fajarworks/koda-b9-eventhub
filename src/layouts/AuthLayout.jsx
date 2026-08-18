11cdcdcc5d0356cf4cf9404bb59c4fb701d160bb
fajarworks <muhammad.fajarr362@gmail.com>
2026-08-18 15:12:27 +0700

    feat:protecting auth routes

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,14 +1,19 @@
 // AuthLayout.jsx
-import { Outlet } from "react-router";
+import { Navigate, Outlet } from "react-router";
 import SidePanel from "../components/SidePanel";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 function AuthLayout() {
+  const [userActive] = useLocalStorage("userActive", null)
+  if (userActive) {
+    return <Navigate to={"/explore"} replace/>
+  }
   return (
     <main className="h-screen overflow-hidden grid grid-cols-1 lg:grid-cols-[35%_65%]">
-      <section className="hidden lg:block h-full overflow-hidden">
+      <section className="bg-radial from-orange-900 from-5% to-black hidden lg:block h-full overflow-hidden">
         <SidePanel />
       </section>
-      <section className="w-full flex items-center justify-center px-3 sm:px-6 lg:px-10">
+      <section className=" w-full flex items-center justify-center px-3 sm:px-6 lg:px-10">
         <Outlet />
       </section>
     </main>
